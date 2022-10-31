7daf3410ff1cad18b552bcca596d209a0f1c8831
Soon Bin Kwon <76522430+Kkwon-SB@users.noreply.github.com>
2022-10-31 16:29:54 +0900

    Update 1.sql

--- Building_hadoop-eco-sys/Package/1.sql
+++ Building_hadoop-eco-sys/Package/1.sql
@@ -1,4 +1,4 @@
-##필수 요소 업데이트 및 설치
+--필수 요소 업데이트 및 설치
 
 sudo apt-get update
 sudo apt-get -y upgrade
@@ -7,32 +7,32 @@ sudo apt-get -y dist-upgrade
 sudo apt-get install -y vim wget unzip ssh openssh-* net-tools tree
 
 
-# Ubuntu 20.04 에는 native library 인 snappy 가 설치되어 있지 않다.
-# 아래 snappy 설치를 하지 않으면 하둡 설치 후 snappy 사용 시 에러가 발생한다.
+-- Ubuntu 20.04 에는 native library 인 snappy 가 설치되어 있지 않다.
+-- 아래 snappy 설치를 하지 않으면 하둡 설치 후 snappy 사용 시 에러가 발생한다.
 sudo apt install libsnappy-dev -y
 
-##Java 설치
+--Java 설치
 sudo apt-get install -y openjdk-8-jdk
 
-# Java Version 확인
+-- Java Version 확인
 java -version
 
-# Java 설치 경로 확인
+-- Java 설치 경로 확인
 sudo find / -name java-8-openjdk-amd64 2>/dev/null
-# /usr/lib/jvm/java-8-openjdk-amd64
+-- /usr/lib/jvm/java-8-openjdk-amd64
 
-##Java 환경 변수 설정
-# Java 시스템 환경변수 등록 및 활성화
+--Java 환경 변수 설정
+-- Java 시스템 환경변수 등록 및 활성화
 $ sudo vim /etc/environment
 
-# 아래 내용 추가 후 저장
+-- 아래 내용 추가 후 저장
 PATH 뒤에 ":/usr/lib/jvm/java-8-openjdk-amd64/bin" 추가
 JAVA_HOME="/usr/lib/jvm/java-8-openjdk-amd64"
 
-# 시스템 환경변수 활성화
+-- 시스템 환경변수 활성화
 $ source /etc/environment
 
-# 사용자 환경변수 등록
+-- 사용자 환경변수 등록
 $ sudo echo 'export JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64' >> ~/.bashrc
 
 # 사용자 환경변수 활성화
